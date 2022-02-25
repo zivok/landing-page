b8b12f0d9a49e0d2ec576aa34077a3723a4d0851
Jhonatan Yupanqui <jnyupanqui.zw@hotmail.com>
2022-02-25 14:54:08 -0800

    add menu

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const sections = document.querySelectorAll("section");
+const nav = document.querySelector("nav");
+
+const menu = document.createElement("ul");
+for (let section of sections) {
+    const a = document.createElement("a");
+    const li = document.createElement("li");
+    li.appendChild(a);
+    menu.appendChild(li);
+    a.href = `#${section.id}`;
+    a.textContent = section.id;
+}
+nav.appendChild(menu);
+
+document.body.addEventListener("click", function(ev) {
+    if (ev.target.matches(".hamburger")) {
+        nav.classList.toggle("open")
+    } else if (nav.classList.contains("open")) {
+        nav.classList.remove("open");
+    }
+})
